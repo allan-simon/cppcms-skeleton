8c2e9e97ef06dcca55a2f017963ece586b895e07
Allan SIMON <allan.simon@supinfo.com>
2012-08-07 01:36:40 +0800

    added a gitignore and finished the init script

--- tools/init.py
+++ tools/init.py
@@ -2,20 +2,61 @@ import config
 import fileinput
 import os
 
+IGNORE_DIRS = [
+    'tools',
+    'build',
+    '.git',
+    '.svn'
+]
+
+
 rootdir = '..'
 
 # we do a recursive walk 
 for path, dirs, files in os.walk(rootdir):
+    # we ignore some directories
+    for ignoreDir in IGNORE_DIRS:
+        if ignoreDir in  dirs:
+            dirs.remove(ignoreDir)
     if files:
         for file in files:
             fullPathFile  = os.path.join(path,file)
+            print(fullPathFile)
             # in all the files we search and replace for the placeholders
             for line in fileinput.FileInput(fullPathFile,inplace=1):
                 for find,replace in config.REPLACEMENT.items():
                     line  = line.replace(find,replace)
                 #note FileInput redirect STDIN to the file that's why
                 # we use print and nothing is displayed
-                print(line)
+                print(line, end='')
+
+
+
+#rename files
+srcDir = os.path.join(
+    os.pardir,
+    'src'
+)
+originalFile = os.path.join (
+    srcDir,
+    '@PROJECT_NAME_CODE@'
+)
+
+renamedFile = os.path.join(
+    srcDir,
+    config.REPLACEMENT['@PROJECT_NAME_CODE@']
+)
+
+os.rename(
+    originalFile + '.h',
+    renamedFile + '.h'
+)
+
+os.rename(
+    originalFile + '.cpp',
+    renamedFile + '.cpp'
+)
+
 
 
 
