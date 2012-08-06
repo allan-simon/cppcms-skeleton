3b5f0c0dc1a15536585480c3eca1c951597f074e
Allan SIMON <allan.simon@supinfo.com>
2012-08-07 07:03:10 +0800

    forget to replace also the application class name

--- tools/config.py
+++ tools/config.py
@@ -1,8 +1,9 @@
-REPLACEMENT = { 
+REPLACEMENT = {
     '@AUTHOR@' : 'your name',
     '@EMAIL@': 'Your_email.com',
     '@PROJECT_NAME_CODE@' : 'your_project_name',
     '@PROJECT_NAME_HUMAN@': 'Your project name',
+    '@MAIN_CLASS@' : 'Name of your application class',
     '@DESCRIPTION@' : 'Description of your project',
     '@PROJECT_WEBSITE' : 'link to your project',
     '@YEARS@' : 'copyright years'
