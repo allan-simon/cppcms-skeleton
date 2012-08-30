f5022e96fa3cb561778bb7f25df0117c505a8135
Allan SIMON <allan.simon@supinfo.com>
2012-08-30 18:22:25 +0800

    modified the tools to generate code so that they can be called by other functions

--- tools/addMethod.py
+++ tools/addMethod.py
@@ -9,98 +9,104 @@ from utils import *
 import os
 import re
 
-parser = ArgumentParser(
-    description = "Add a method to a controller"
-)
+def addMethod(controller, method, description):
+    controllerInclude = controller.upper()
 
-parser.add_argument(
-    'controller',
-    metavar = 'CONTROLLER',
-    help = 'name of the controller in which you want to add a method'
-)
+    controllerUnderscore = camelToUnderscore(controller)
+    viewName =controllerUnderscore + "_" + method 
 
-parser.add_argument(
-    'method',
-    metavar = 'METHOD',
-    help = 'name of the method you want to add'
-)
-
-
-
-parser.add_argument(
-    '-d',
-    '--description',
-    nargs='?',
-    help = 'a description of what your method is supposed to do',
-    default = '@TODO add a description'
-)
-
-args = parser.parse_args();
-
-
-
-description = args.description
-#TODO maybe do some check if the user enter a non valid
-# controller name
-controller = args.controller
-method= args.method
-controllerInclude = controller.upper()
-
-controllerUnderscore = camelToUnderscore(controller)
-viewName =controllerUnderscore + "_" + method 
-
-replacePlaceholders = {
-    '%%CONTROLLER_NAME%%' : controller,
-    '%%ACTION_NAME%%' : method,
-    #NOTE: it seems that - is more Google friendly than _
-    '%%ACTION_URL%%' : '/%s' % (method.replace('_','-')),
-    '%%ACTION_TODAY%%' : date.today().strftime('%d %B %Y'),
-    '%%ACTION_DESCRIPTION%%' : description,
-    '%%CONTENT_NAME%%' : underscoreToPascalCase(method),
-    '%%CONTROLLER_NS%%' : controllerUnderscore,
-    '%%VIEW_NAME%%' : viewName
-}
+    replacePlaceholders = {
+        '%%CONTROLLER_NAME%%' : controller,
+        '%%ACTION_NAME%%' : method,
+        #NOTE: it seems that - is more Google friendly than _
+        '%%ACTION_URL%%' : '/%s' % (method.replace('_','-')),
+        '%%ACTION_TODAY%%' : date.today().strftime('%d %B %Y'),
+        '%%ACTION_DESCRIPTION%%' : description,
+        '%%CONTENT_NAME%%' : underscoreToPascalCase(method),
+        '%%CONTROLLER_NS%%' : controllerUnderscore,
+        '%%VIEW_NAME%%' : viewName
+    }
 
 
 
 # first edit the cpp file
-insertFromTemplate(
-    os.path.join(CTRL_OUTPUT_DIR,controller + '.cpp'),
-    NEXT_ACTION_DISPATCHER_MARKER,
-    os.path.join(CTRL_TEMPLATE_DIR,TMPL_METHOD_DISPATCH_CPP),
-    replacePlaceholders,
-)
+    insertFromTemplate(
+        os.path.join(CTRL_OUTPUT_DIR,controller + '.cpp'),
+        NEXT_ACTION_DISPATCHER_MARKER,
+        os.path.join(CTRL_TEMPLATE_DIR,TMPL_METHOD_DISPATCH_CPP),
+        replacePlaceholders,
+    )
 
-insertFromTemplate(
-    os.path.join(CTRL_OUTPUT_DIR,controller + '.cpp'),
-    NEXT_ACTION_MARKER,
-    os.path.join(CTRL_TEMPLATE_DIR,TMPL_METHOD_CPP),
-    replacePlaceholders,
-)
+    insertFromTemplate(
+        os.path.join(CTRL_OUTPUT_DIR,controller + '.cpp'),
+        NEXT_ACTION_MARKER,
+        os.path.join(CTRL_TEMPLATE_DIR,TMPL_METHOD_CPP),
+        replacePlaceholders,
+    )
 
 
 # then the .h file
-insertFromTemplate(
-    os.path.join(CTRL_OUTPUT_DIR,controller + '.h'),
-    NEXT_ACTION_MARKER,
-    os.path.join(CTRL_TEMPLATE_DIR,TMPL_METHOD_H),
-    replacePlaceholders
-)
+    insertFromTemplate(
+        os.path.join(CTRL_OUTPUT_DIR,controller + '.h'),
+        NEXT_ACTION_MARKER,
+        os.path.join(CTRL_TEMPLATE_DIR,TMPL_METHOD_H),
+        replacePlaceholders
+    )
 
 
 # edit the content header to add the content for that action
-insertFromTemplate(
-    os.path.join(CONTENT_OUTPUT_DIR,controller + '.h'),
-    NEXT_CONTENT_MARKER,
-    os.path.join(CONTENT_TMPL_DIR,TMPL_CONTENT_INCLUDE_H),
-    replacePlaceholders
-)
+    insertFromTemplate(
+        os.path.join(CONTENT_OUTPUT_DIR,controller + '.h'),
+        NEXT_CONTENT_MARKER,
+        os.path.join(CONTENT_TMPL_DIR,TMPL_CONTENT_INCLUDE_H),
+        replacePlaceholders
+    )
 
 
 # create the view for that action
 
-generateFromTemplate(
-    os.path.join(VIEW_CTRL_TMPL_DIR,TMPL_VIEW_ACTION_TPL),
-    replacePlaceholders,
-    os.path.join(VIEW_CTRL_OUTPUT_DIR,controllerUnderscore,method + '.tmpl')
-)
+    generateFromTemplate(
+        os.path.join(VIEW_CTRL_TMPL_DIR,TMPL_VIEW_ACTION_TPL),
+        replacePlaceholders,
+        os.path.join(VIEW_CTRL_OUTPUT_DIR,controllerUnderscore,method + '.tmpl')
+    )
+
+if __name__ == '__main__':
+    parser = ArgumentParser(
+        description = "Add a method to a controller"
+    )
+
+    parser.add_argument(
+        'controller',
+        metavar = 'CONTROLLER',
+        help = 'name of the controller in which you want to add a method'
+    )
+
+    parser.add_argument(
+        'method',
+        metavar = 'METHOD',
+        help = 'name of the method you want to add'
+    )
+
+
+
+    parser.add_argument(
+        '-d',
+        '--description',
+        nargs='?',
+        help = 'a description of what your method is supposed to do',
+        default = '@TODO add a description'
+    )
+
+    args = parser.parse_args();
+
+
+
+    description = args.description
+#TODO maybe do some check if the user enter a non valid
+# controller name
+    controller = args.controller
+    method= args.method
+
+
+    addMethod(controller, method, description)
--- tools/addModel.py
+++ tools/addModel.py
@@ -11,51 +11,55 @@ from utils import *
 from config import REPLACEMENT
 
 
-parser = ArgumentParser(
-    description = "Add a new model"
-)
+def addModel(model,description):
+    modelInclude = model.upper()
 
-parser.add_argument(
-    'model',
-    metavar = 'MODEL',
-    help = 'name of model you want to add'
-)
+    replacePlaceholders = {
+        '%%MODEL_NAME%%' : model,
+        '%%MODEL_INCLUDE%%' : modelInclude,
+        '%%MODEL_TODAY%%' : date.today().strftime('%d %B %Y'),
+        '%%MODEL_DESCRIPTION%%' : description
+    }
 
-parser.add_argument(
-    '-d',
-    '--description',
-    nargs='?',
-    help = 'a description of what your model is supposed to do',
-    default = '@TODO add a description'
-)
+    # first edit the cpp file
+    generateFromTemplate(
+        os.path.join(MODEL_TMPL_DIR,MODEL_CPP_TPL),
+        replacePlaceholders,
+        os.path.join(MODEL_OUTPUT_DIR,model + '.cpp')
+    )
 
+    # then the .h file
+    generateFromTemplate(
+        os.path.join(MODEL_TMPL_DIR,MODEL_H_TPL),
+        replacePlaceholders,
+        os.path.join(MODEL_OUTPUT_DIR,model + '.h')
+    )
 
-args = parser.parse_args();
+if __name__ == '__main__':
 
+    parser = ArgumentParser(
+        description = "Add a new model"
+    )
 
-description = args.description
-model = args.model
-modelInclude = model.upper()
+    parser.add_argument(
+        'model',
+        metavar = 'MODEL',
+        help = 'name of model you want to add'
+    )
 
-replacePlaceholders = {
-    '%%MODEL_NAME%%' : model,
-    '%%MODEL_INCLUDE%%' : modelInclude,
-    '%%MODEL_TODAY%%' : date.today().strftime('%d %B %Y'),
-    '%%MODEL_DESCRIPTION%%' : description
-}
-
-# first edit the cpp file
-generateFromTemplate(
-    os.path.join(MODEL_TMPL_DIR,MODEL_CPP_TPL),
-    replacePlaceholders,
-    os.path.join(MODEL_OUTPUT_DIR,model + '.cpp')
-)
-
-# then the .h file
-generateFromTemplate(
-    os.path.join(MODEL_TMPL_DIR,MODEL_H_TPL),
-    replacePlaceholders,
-    os.path.join(MODEL_OUTPUT_DIR,model + '.h')
-)
+    parser.add_argument(
+        '-d',
+        '--description',
+        nargs='?',
+        help = 'a description of what your model is supposed to do',
+        default = '@TODO add a description'
+    )
 
 
+    args = parser.parse_args();
+
+
+    description = args.description
+    model = args.model
+
+    addModel(model,description)
