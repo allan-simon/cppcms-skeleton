9e1b1beaea66fa718549d71dd7f0588f5e6aa020
Allan SIMON <allan.simon@supinfo.com>
2013-01-13 05:10:28 +0800

    added a new test for the functions models::SqliteModel::import_sql_file that for the moment fails

---
+++ tests/bug_insert.sql
@@ -0,0 +1,23 @@
+-- fill the articles table with some values
+INSERT INTO articles(
+    lang,
+    slug,
+    title,
+    content
+) VALUES(
+    "fr",
+    "page_principale",
+    "Page Principale",
+    "Juste pour voir si ça marche"
+);
+INSERT INTO articles(
+    lang,
+    slug,
+    title,
+    content
+) VALUES(
+    "fr",
+    "français",
+    "Langue Française",
+    "Et un deuxième article"
+);
---
+++ tests/models_sqlitemodel_import_sql_file_test2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include "models/SqliteModel.h" 
+#include "test.h"
+
+#define OK "[OK]"
+#define FAIL "[FAILED]"
+
+#define SQL_PATH "../tests/" 
+#define TEST_NAME "models_sqlitemodel_import_sql_file_test"
+#define BUG_INSERT_SQL "bug_insert.sql"
+#define BUG_INSERT_CREATE_TABLES_SQL "bug_insert_create_tables.sql"
+int main() {
+
+    // the goal here is to test the following case
+    // 
+    // an import containing INSERT fails with a unique constraint
+    // errors though the table is empty and the import works 
+    // when done directly (i.e using  sqlite3 my.db < the_file.sql
+    bool nothingFailed = true;
+
+    models::SqliteModel model("test2.db");
+
+    // we first create the database
+    CPPCMSSKEL_TEST_RESULT_WORK(
+        "Try to create the tables ... " ,
+        model.import_sql_file(
+          SQL_PATH BUG_INSERT_CREATE_TABLES_SQL
+        ),
+        nothingFailed
+    );
+
+    // and we load the data
+    CPPCMSSKEL_TEST_RESULT_WORK(
+        "Now we inset some data",
+        model.import_sql_file(
+          SQL_PATH BUG_INSERT_SQL
+        ),
+        nothingFailed
+    );
+
+
+    if (nothingFailed) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
