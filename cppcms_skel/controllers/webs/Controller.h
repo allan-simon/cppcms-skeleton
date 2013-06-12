226e00193b92b7ae261ba757fd1e8270495d70a4
Allan SIMON <allan.simon@supinfo.com>
2013-06-12 09:01:12 +0800

    moved the licence information to a dedicated file, so that we don't have a load of licence text at the beginnings of each files

--- cppcms_skel/controllers/webs/Controller.h
+++ cppcms_skel/controllers/webs/Controller.h
@@ -1,38 +1,12 @@
 /**
- * cppcms-skeleton a set of tools and code over cppcms
- * Copyright (C) 2012 Allan SIMON <allan.simon@supinfo.com>
- * 
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom
- * the Software is furnished to do so, subject to the following
- * conditions:
- * 
- * The above copyright notice and this permission notice shall
- * be included in all copies or substantial portions of the
- * Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
- * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
- * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Copyright (C) 2012-2013 Allan SIMON (Sysko) <allan.simon@supinfo.com>
+ * See accompanying file COPYING.TXT file for licensing details.
  *
  * @category Cppcms-skeleton
  * @author   Allan SIMON <allan.simon@supinfo.com>
  * @package  Controllers
- * @license  MIT
- * @link     https://github.com/allan-simon/cppcms-skeleton
  *
  */
-
-
 
 #ifndef CPPCMSSKEL_CONTROLLERS_WEBS_CONTROLLER_H
 #define CPPCMSSKEL_CONTROLLERS_WEBS_CONTROLLER_H
--- cppcms_skel/models/Users.cpp
+++ cppcms_skel/models/Users.cpp
@@ -1,28 +1,12 @@
 /**
- * cppcms-skeleton a set of tools and code over cppcms
- * Copyright (C) 2012 Allan SIMON <allan.simon@supinfo.com> 
+ * Copyright (C) 2012-2013 Allan SIMON (Sysko) <allan.simon@supinfo.com>
+ * See accompanying file COPYING.TXT file for licensing details.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- * @category cppcmsskel
+ * @category Cppcms-skeleton
+ * @author   Allan SIMON <allan.simon@supinfo.com>
  * @package  Models
- * @author   Allan SIMON <allan.simon@supinfo.com> 
- * @license  Affero General Public License
- * @link     https://github.com/sysko/cppcms-skeleton
+ *
  */
-
 
 #include <iostream>
 #include <string>
